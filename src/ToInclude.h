683e8a6e88fd010c07ebaf2ae689e24a5c4cd9f3
DrEden33773 <eden01960@nuaa.edu.cn>
2022-09-24 17:17:06 +0800

    better comments

--- src/ToInclude.h
+++ src/ToInclude.h
@@ -1,7 +1,9 @@
 ﻿/**
  * @file ToInclude.h
  * @author Eden (edwardwang33773@gmail.com)
- * @brief A file which includes => <bits/stdc++.h>
+ * @brief
+        A file which includes => <bits/stdc++.h>
+        And, added `ifndef define endif` to avoid multi-compilation
  * @version 0.1
  * @date 2022-09-24
  *
